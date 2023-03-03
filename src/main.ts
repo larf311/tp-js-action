79881f2211ae33aba3691406b5a3e1372857a939
Trevor Paddock <trevor.m.paddock@gmail.com>
2023-03-03 17:41:00 -0500

    Try to log commit info.

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,39 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 import {wait} from './wait'
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+  // try {
+  //   const ms: string = core.getInput('milliseconds')
+  //   core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+  //   core.debug(new Date().toTimeString())
+  //   await wait(parseInt(ms, 10))
+  //   core.debug(new Date().toTimeString())
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
-  }
+  //   core.setOutput('time', new Date().toTimeString())
+  // } catch (error) {
+  //   if (error instanceof Error) core.setFailed(error.message)
+  // }
+
+  // This should be a token with access to your repository scoped in as a secret.
+  // The YML workflow will need to set myToken with the GitHub Secret Token
+  // myToken: ${{ secrets.GITHUB_TOKEN }}
+  // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
+  const myToken = core.getInput('myToken')
+
+  const octokit = github.getOctokit(myToken)
+
+  // You can also pass in additional options as a second parameter to getOctokit
+  // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
+
+  const { data: commit } = await octokit.rest.git.getCommit({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    commit_sha: github.context.sha
+  })
+
+  console.log(commit)
 }
 
 run()
